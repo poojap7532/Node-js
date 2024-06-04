61079d0012d0a94c1fd17f4b48ab4b5f28ff00e5
poojap7532 <130437269+poojap7532@users.noreply.github.com>
2024-06-04 09:43:52 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-const http = require('http');
-http.createServer((req,res)=>{
-    res.write("hello this is pooja");
-    res.end()
-}).listen(3001);
+require("dotenv").config
+let hhtp = require("http");
+let express = require("express");
+let bodyparser = require("body-parser")
+let cors = require("cors")
+const route = require("./router");
+const dbConnect = require("./db/connectDB");
+let app = express();
+
+
+// use cors for all desktop
+app.use(cors({
+    origin: "*"
+}))
+
+//for json body express
+// app.use(express.urlencoded({extends:false}));
+// app.use(express.json())
+
+//for json body use bodyparser
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+
+//uisng routes
+app.use("/v1", route);
+
+//coonect databaes
+dbConnect();
+
+//create server
+let http = hhtp.createServer(app).listen(3001, () => {
+    console.log("create servever succesfully");
+})
+
+
